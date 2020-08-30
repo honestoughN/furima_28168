a80a355ec53abba38377520e659504d00cc8b5bf
honestoughN <honest.tough18@gmail.com>
2020-08-30 12:26:17 +0900

    ユーザー本名のカナひらがな漢字のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /@/ }
   validates :password, presence: true, format: { with: /\A[a-z\d]{6,}\z/i }
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, presence: true, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/ }
   validates :date_of_birth, presence: true
 
 end
