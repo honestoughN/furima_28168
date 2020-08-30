f537e0139a59686432967de37c052e4e6dc25c04
honestoughN <honest.tough18@gmail.com>
2020-08-30 11:37:30 +0900

    メールアドレスに＠が必須のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniquness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /@/ }
   validates :password, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
