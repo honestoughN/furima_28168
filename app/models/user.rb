f8e0cc90cb1b1acf6a8af08da2b195c2b2f8617d
honestoughN <honest.tough18@gmail.com>
2020-08-30 11:40:06 +0900

    パスワードの６文字以上のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /@/ }
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
