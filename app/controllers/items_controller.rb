0d5443d5c4e2893e38de93f0a68e436dad101c45
honestoughN <honest.tough18@gmail.com>
2020-08-30 12:50:24 +0900

    ログインしていない時の、ログイン画面へのリダイレクト設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, only: [:new]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -6,5 +7,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  private
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 end
