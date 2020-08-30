e26df7640f7f07c2ae170dc07b3f5b06f3e5c8f3
honestoughN <honest.tough18@gmail.com>
2020-08-30 12:32:58 +0900

    ユーザー情報の入力失敗時のエラー文の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /@/ }
-  validates :password, presence: true, format: { with: /\A[a-z\d]{6,}\z/i }
-  validates :first_name, presence: true, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/ }
-  validates :last_name, presence: true, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/ }
+  validates :email, presence: true, uniqueness: true, format: { with: /@/, message: "@を含めて入力して下さい" }
+  validates :password, presence: true, format: { with: /\A[a-z\d]{6,}\z/i, message: "半角英数字6文字以上で入力して下さい" }
+  validates :first_name, presence: true, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/, message: "全角(ひらがな、カタカナ、漢字)で入力して下さい" }
+  validates :last_name, presence: true, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/, message: "全角(ひらがな、カタカナ、漢字)で入力して下さい" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/, message: "全角カタカナで入力して下さい" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/, message: "全角カタカナで入力して下さい" }
   validates :date_of_birth, presence: true
 
 end
