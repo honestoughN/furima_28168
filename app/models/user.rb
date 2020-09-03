8a032518a719ab97ae5b36c1fcb6d9e9695aca75
honestoughN <honest.tough18@gmail.com>
2020-09-03 20:35:04 +0900

    コメントアウト削除、with_optionsの使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /@/, message: "@を含めて入力して下さい" }
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6,}\z/i, message: "半角英数字6文字以上で入力して下さい" }
-  validates :first_name, presence: true, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/, message: "全角(ひらがな、カタカナ、漢字)で入力して下さい" }
-  validates :last_name, presence: true, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/, message: "全角(ひらがな、カタカナ、漢字)で入力して下さい" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/, message: "全角カタカナで入力して下さい" }
-  validates :last_name_kana, presence: true, format: { with: /\A[ア-ン]+\z/, message: "全角カタカナで入力して下さい" }
-  validates :date_of_birth, presence: true
-
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true, format: { with: /@/, message: "@を含めて入力して下さい" }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+{6,}\z/i, message: "半角英数字6文字以上で入力して下さい" }
+    validates :first_name, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/, message: "全角(ひらがな、カタカナ、漢字)で入力して下さい" }
+    validates :last_name, format: { with: /\A[あ-ん ア-ン 一-龥]+\z/, message: "全角(ひらがな、カタカナ、漢字)で入力して下さい" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" }
+    validates :date_of_birth
+  end
 end
