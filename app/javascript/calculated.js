3d60ec301c9c3b3e2f5b271565914f28bafee9d4
honestoughN <honest.tough18@gmail.com>
2020-09-05 23:44:39 +0900

    販売手数料、利益の表示の設定

--- app/javascript/calculated.js
+++ app/javascript/calculated.js
@@ -1,4 +1,23 @@
-function calculate() {
-  const calculateDisplay = document.getElementsByClassName("hidden")
-  console.log(calculateDisplay)
+window.addEventListener('load', price);
+
+function price(){
+  const priceInput = document.getElementById("item-price");
+  const add_tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  priceInput.addEventListener('keyup', () => {
+    const value = priceInput.value;
+
+    if (value >= 300 && value <= 9999999){
+      let fee = value * 0.1
+      let gains = value - fee
+      add_tax.textContent = fee;
+      profit.textContent = gains;
+    } else {
+      let fee = 'NaN';
+      let gains = 'NaN';
+      add_tax.textContent = fee;
+      profit.textContent = gains;
+    }
+  });
 }
