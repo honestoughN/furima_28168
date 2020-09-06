6d648216e6bce20477b435d9e1557cbfc9329db7
honestoughN <honest.tough18@gmail.com>
2020-09-06 11:34:56 +0900

    itemモデルのテストコード記入

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,56 +10,93 @@ RSpec.describe Item, type: :model do
       it "image, name, detail, category, status, shipping_charge, shipping_area, shipping_day, selling_priceがあれば登録出品できる" do
         expect(@item).to be_valid
       end
-
+    end
     context '出品登録がうまくいかないとき' do
       it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'nameが空だと出品できない' do
-        
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it 'detailが空だと出品できない' do
-        
+        @item.detail = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Detail can't be blank")
       end
       it 'categoryが空だと出品できない' do
-        
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'categoryが---だと出品できない' do
-        
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it 'statusが空だと出品できない' do
-        
+        @item.status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it 'statusが---だと出品できない' do
-        
+        @item.status_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
       it 'shipping_chargeが空だと出品できない' do
-        
+        @item.shipping_charge_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge can't be blank")
       end
       it 'shipping_chargeが---だと出品できない' do
-        
+        @item.shipping_charge_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
       end
       it 'shippin_areaが空だと出品できない' do
-        
+        @item.shipping_area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area can't be blank")
       end
       it 'shipping_areaが---だと出品できない' do
-        
+        @item.shipping_area_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
       end
       it 'shipping_dayが空だと出品できない' do
-        
+        @item.shipping_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
       it 'shipping_dayが---だと出品できない' do
-        
+        @item.shipping_day_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
       end
       it 'selling_priceが空だと出品できない' do
-        
+        @item.selling_price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price can't be blank")
       end
       it 'selling_priceが299以下だと出品できない' do
-        
+        @item.selling_price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price must be greater than or equal to 300")
       end
       it 'selling_priceが10000000以上だと出品できない' do
-        
+        @item.selling_price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price must be less than or equal to 9999999")
       end
-    end
+      it 'selling_priceが全角数字だと出品できない' do
+        @item.selling_price = "４００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Selling price is not a number")
+      end
     end
   end
 end
