77a8923120460328eb7bfaaa754681dd1b5bedc6
honestoughN <honest.tough18@gmail.com>
2020-09-12 20:57:31 +0900

    配送先住所の県のactive:hashの設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,13 @@
 class ShippingAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :order
+
+  with_options presence: true do
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :postal_code
+    validates :city
+    validates :block_number
+    validates :phone_number
+  end
 end
