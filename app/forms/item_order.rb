77027245bec9138b25c558afe3f9edad3a286228
honestoughN <honest.tough18@gmail.com>
2020-09-13 21:39:58 +0900

    購入時の配送先などの情報保存設定

--- app/forms/item_order.rb
+++ app/forms/item_order.rb
@@ -4,8 +4,8 @@ class ItemOrder
   attr_accessor :postal_code, :prefecture_id, :city, :block_number, :building_name, :phone_number, :item_id, :user_id
 
   def save
-    order = Order.create
-    Item.create(order_id: order.id)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Item.update(order_id: order.id)
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block_number: block_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 
