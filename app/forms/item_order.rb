500551322a2cdfe6be963884e8efa7f36fd954e1
honestoughN <honest.tough18@gmail.com>
2020-09-18 22:27:15 +0900

    購入済みアイテムへのorder_id取り付け（購入有無判断のため）

--- app/forms/item_order.rb
+++ app/forms/item_order.rb
@@ -1,11 +1,12 @@
 class ItemOrder
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :block_number, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :block_number, :building_name, :phone_number, :item_id, :user_id, :order_id
 
   def save
+    item = Item.find(item_id)
     order = Order.create(item_id: item_id, user_id: user_id)
-    Item.update(order_id: order.id)
+    item.update(order_id: order.id)
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block_number: block_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 
