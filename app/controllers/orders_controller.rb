7aa366733590c8459fcedadb5ce1894b1d621a21
honestoughN <honest.tough18@gmail.com>
2020-09-19 19:36:34 +0900

    直接URL入力した場合の一覧ページへの遷移設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :move_to_sign_in
   before_action :set_item, only: [:new, :create]
+  before_action :move_to_index
  
   def new
     @order = ItemOrder.new
@@ -18,6 +19,12 @@ class OrdersController < ApplicationController
   end
 
   private
+  def move_to_index
+    unless current_user.id != @item.user.id || @item.order.blank?
+      redirect_to root_path
+    end
+  end
+
   def move_to_sign_in
     unless user_signed_in?
       redirect_to new_user_session_path
