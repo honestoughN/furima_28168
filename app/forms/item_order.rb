a600424469beb84b337a9348dc24ce4487e8ecdb
honestoughN <honest.tough18@gmail.com>
2020-09-19 09:37:55 +0900

    ItemOrderへのバリデーション設定

--- app/forms/item_order.rb
+++ app/forms/item_order.rb
@@ -3,6 +3,14 @@ class ItemOrder
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :block_number, :building_name, :phone_number, :item_id, :user_id, :order_id
 
+  with_options presence: true do
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message:  "is invalid. Include hyphen(-)" }
+    validates :city
+    validates :block_number
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: "ハイフンを除いた11桁で入力してください" }
+  end
+
   def save
     item = Item.find(item_id)
     order = Order.create(item_id: item_id, user_id: user_id)
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -3,11 +3,4 @@ class ShippingAddress < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :order
 
-  with_options presence: true do
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :postal_code
-    validates :city
-    validates :block_number
-    validates :phone_number
-  end
 end
