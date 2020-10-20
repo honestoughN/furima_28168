3df2415b6b19bd33ad96e117bd7eda16197c70d8
honestoughN <honest.tough18@gmail.com>
2020-10-20 21:06:48 +0900

    コントローラーの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def new
+    @comments = Comment.all
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(text: params[:comment][:comment])
   end
 end
